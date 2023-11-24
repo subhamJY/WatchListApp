b16fd17c4eb4641825982cc506535f44330a484f
Subham Saswat Barick <131545766+subhamJY@users.noreply.github.com>
2023-11-24 20:55:15 +0530

    Add files via upload
    java spring project

---
+++ MovieWatchListApp/src/main/java/com/example/subham/watchlist/MovieWatchListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.subham.watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWatchListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWatchListAppApplication.class, args);
+	}
+
+}
